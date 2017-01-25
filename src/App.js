93792ef45d08ce8bb3caf57b53d3eb9177bd06a7
charlesjamb <charles.jambon@gmail.com>
2017-01-25 18:08:39 +0100

    still trying to call Marvel API successfully

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CharactersContainer from './components/CharactersContainer.js';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>The Marvel Characters</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <CharactersContainer/>
+        </div>
       </div>
     );
   }
---
+++ src/components/CharactersContainer.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {Grid, Row} from 'react-bootstrap';
+import {createHash} from 'crypto';
+import axios from 'axios';
+
+export default class CharactersContainer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			characters: []
+		}
+	}
+
+	componentDidMount() {
+		this._getCharacters();
+	}
+
+	_getCharacters() {
+		let publicKey = '298bab46381a6daaaee19aa5c8cafea5';
+		let privateKey = 'b0223681fced28de0fe97e6b9cd091dd36a5b71d';
+		let currentTime = + new Date();
+		let concatenatedString = currentTime + privateKey + publicKey;
+		axios.get(`http://gateway.marvel.com/v1/public/characters`, {
+			params: {
+				"ts": + currentTime,
+				"apiKey": publicKey,
+				"hash": createHash('md5').update(concatenatedString).digest('hex') 
+			}
+		})
+		.then(result => {
+			console.log('request sent');
+			this.setState({
+				characters: result
+			});
+		})
+		.catch(err => {
+			console.log(err.stack);
+		})
+	}
+
+	render() {
+		console.log('Characters list ', this.state.characters)
+		return (
+			<Grid>
+				<Row>
+					<p>Hello, world!</p>
+				</Row>
+			</Grid>
+		);
+	}
+};
