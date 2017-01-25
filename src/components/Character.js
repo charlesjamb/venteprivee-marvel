e0e03a5bcd6d28466440d75d39b7449318b821b4
charlesjamb <charles.jambon@gmail.com>
2017-01-26 00:54:52 +0100

    character components fixed img and responsiveness

--- src/components/Character.js
+++ src/components/Character.js
@@ -5,12 +5,10 @@ export default class Character extends Component {
 	render() {
 		let character = this.props.character;
 		return (
-			<Col xs={6} md={4}>
-				<Thumbnail src={character.thumbnail.path} alt="242x200">
+			<Col xs={12} sm={6} md={4}>
+				<Thumbnail src={character.thumbnail.path + '.' + character.thumbnail.extension} alt="242x200">
 					<h2>{character.name}</h2>
-					<p>
-						<Button bsStyle="default">More info</Button>
-					</p>
+					<Button bsStyle="default" bsSize="large">More info</Button>
 				</Thumbnail>
 			</Col>
 		);
